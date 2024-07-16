7cc8c3430d2e63db8c7e9439be7d612feb0925c1
Sebastien Larue <sebastienlar619@gmail.com>
2024-07-16 15:19:10 +0400

    Added AddCustomer Vehicle form

--- src/app/components/customer-vehicles/customer-vehicles-add/customer-vehicles-add.component.ts
+++ src/app/components/customer-vehicles/customer-vehicles-add/customer-vehicles-add.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DialogService, DynamicDialogComponent, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { CustomerVehiclesService } from '../../../services/customer-vehicles.service';
+import { VehicleImagesService } from '../../../services/vehicle-images.service';
+import { CreateVehicleRequest } from '../../../interfaces/customer-vehicles';
+import { VehicleImageResponse } from '../../../interfaces/vehicle-images';
 
 @Component({
   selector: 'app-customer-vehicles-add',
@@ -6,5 +11,54 @@ import { Component } from '@angular/core';
   styleUrl: './customer-vehicles-add.component.scss'
 })
 export class CustomerVehiclesAddComponent {
+
+  vehiclesData = new Array<VehicleImageResponse>();
+  loading: boolean = true;
+  instance: DynamicDialogComponent | undefined;
+  newCustomerVehicleForm: CreateVehicleRequest = {
+    make: '',
+    model: '',
+    year: '',
+    chassisNo: '',
+    customerId: -1
+  }
+
+  constructor(public dialog: DynamicDialogRef, private dialogService: DialogService,
+              private customerVehiclesService: CustomerVehiclesService, private vehicleImagesService: VehicleImagesService) {
+                this.instance = this.dialogService.getInstance(this.dialog)
+              }
+
+  ngOnInit(): void {
+    this.getAllVehicles();
+  }
+
+  getAllVehicles() : void {
+    this.vehicleImagesService.getAllVehicleImages().subscribe({
+      next: result => {
+        this.vehiclesData = result;
+        this.loading = false;
+      },
+      error: error => {
+        console.error('Error fetching JSON data:', error)
+      }
+    })
+  }
+
+  addCustomerVehicle(form: any) : void {
+    if(!form.valid){
+      return;
+    }
+
+    if(form.valid){
+      this.newCustomerVehicleForm.customerId = this.instance?.data['customerId'];
+      this.customerVehiclesService.addCustomerVehicle(this.newCustomerVehicleForm).subscribe(result => {
+        this.dialog.close(result);
+      })
+    }
+  }
+
+  cancel() {
+    this.dialog.close("Close");
+  }
 
 }
--- src/app/components/customer-vehicles/customer-vehicles-list/customer-vehicles-list.component.ts
+++ src/app/components/customer-vehicles/customer-vehicles-list/customer-vehicles-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { CustomerVehiclesService } from '../../../services/customer-vehicles.service';
-import { VehicleResponse } from '../../../interfaces/customer-vehicles';
 import { map } from 'rxjs';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MessageService } from 'primeng/api';
+import { CustomerVehiclesService } from '../../../services/customer-vehicles.service';
+import { CustomerVehiclesAddComponent } from '../customer-vehicles-add/customer-vehicles-add.component';
+import { VehicleResponse } from '../../../interfaces/customer-vehicles';
 
 @Component({
   selector: 'app-customer-vehicles-list',
@@ -14,9 +17,11 @@ export class CustomerVehiclesListComponent {
   @Input() customerId: number = -1;
 
   customerVehiclesData = new Array<VehicleResponse>();
+  dialog: DynamicDialogRef | undefined;
   responsiveOptions: any[] | undefined;
 
-  constructor(private sanitizer: DomSanitizer, private customerVehiclesService: CustomerVehiclesService) {}
+  constructor(private sanitizer: DomSanitizer, public dialogService: DialogService, 
+              private messageService: MessageService, private customerVehiclesService: CustomerVehiclesService) {}
 
   ngOnInit(): void{
     this.getCustomerVehiclesById(this.customerId);
@@ -49,8 +54,36 @@ export class CustomerVehiclesListComponent {
         });
       })
     ).subscribe((sanitizedResult: VehicleResponse[]) => {
-      console.log(sanitizedResult);
       this.customerVehiclesData = sanitizedResult;
     });
+  }
+
+  addNewCustomerVehicle() : void {
+    this.dialog = this.dialogService.open(CustomerVehiclesAddComponent, {
+      header: 'Add New Vehicle',
+      width: '50vw',
+      modal: true,
+      data: {
+        customerId: this.customerId
+      },
+      breakpoints: {
+          '960px': '75vw',
+          '640px': '90vw'
+      },
+    });
+
+    this.dialog.onClose.subscribe((data: any) => {
+      if(data !== "Close"){
+        const vehicleData = data as VehicleResponse[];
+        const sanitizedData = vehicleData.map((vehicle: VehicleResponse) => {
+          vehicle.vehicleImage = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/jpeg;base64, ${vehicle.vehicleImage}`) as string;
+          return vehicle;
+        });
+
+        this.customerVehiclesData = sanitizedData;
+        this.messageService.clear();
+        this.messageService.add({ key: 'successKey', severity: 'success', summary: 'Success', detail: 'Vehicle added successfully' });
+      }
+    })
   }
 }
